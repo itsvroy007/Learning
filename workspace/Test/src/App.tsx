17be4256bc497318fa3670e82218b8a49c37bdbe
darpan <darpan@darpans-MacBook-Air.local>
2026-06-25 18:06:26 +0530

    New proj added

---
+++ workspace/Test/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { ChatInput } from './components/ChatInput';
+import ChatMessages from './components/ChatMessages';
+import './App.css'
+
+function App() {
+  const [chatMessages, setChatMessages] = useState([
+    {
+      message: "hello Chatbot",
+      sender: "user",
+      id: "id1",
+    },
+    {
+      message: "Hello How can I help you?",
+      sender: "robot",
+      id: "id2",
+    },
+    {
+      message: "Can you get me todays date?",
+      sender: "user",
+      id: "id3",
+    },
+    {
+      message: "Today is ",
+      sender: "robot",
+      id: "id4",
+    },
+  ]);
+  //const chatMessages= array[0];
+  // const setChatMessage= array[1];
+
+  return (
+    <div className="app-container">
+      <ChatMessages chatMessages={chatMessages} />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+  );
+}
+export default App
