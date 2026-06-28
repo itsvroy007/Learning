eddf1785385c3be75213b2afbf6ada7c96deb615
itsvroy007 <itsvroy007@duck.com>
2026-06-28 14:36:59 +0530

    added starting code files

---
+++ RPSproject.py
@@ -0,0 +1,21 @@
+
+list1 = []
+
+''' 
+# ssh bandit0@bandit.labs.overthewire.org -p 2220
+lvl 0 - bandit0
+lvl 0 => 1 - ZjLjTmM6FvvyRnrb2rfNWOZOTa6ip5If
+lvl 1 => 2 - 263JGJPfgU6LtdEvgfWU1XP5yac29mFx
+lvl 2 => 3 - MNk8KNH3Usiio41PRUEoDFPqfxLPlSmx
+lvl 3 => 4 - 2WmrDFRmJIq3IPxneAaMGhap0pFhF3NJ
+lvl 4 => 5 - 4oQYVPkxZOOEOO5pTW81FB8j8lxXGUQw
+lvl 5 => 6 - HWasnPhtq9AVKe0dmk45nxy20cvUa6EG
+lvl 6 => 7 - morbNTDkSW6jIlUc0ymOdMaLnOlFVAaj
+lvl 7 => 8 - dfwvzFQi4mU0wfNbFOe9RoWskMLg7eEc
+lvl 8 => 9 - 4CKMh1JI91bUIZZPXDqGanal4xvAg0JM
+lvl 9 => 10 - FGUW5ilLVJrxX9kMYMmlN4MgbpfMiqey
+lvl 10 => 11 - dtR173fZKb0RRsDFSGsg2RWnpNVj3qRr
+lvl 11 => 12 - 7x16WNeHIi5YkIhWsfFIqoognUTyj9Q4
+lvl 12 => 13 - 
+
+'''
---
+++ workspace/LEARNING/React/ecommerce-project/src/App.jsx
@@ -0,0 +1,24 @@
+import  { HomePage }   from './pages/HomePage'
+import  {CheckoutPage}  from './pages/CheckoutPage'
+import { Routes , Route }  from 'react-router' 
+import  OrdersPage  from './pages/OrdersPage'
+import TrackingPage  from './pages/TrackingPage'
+
+
+function App() {
+  return (
+    <>
+      <p>heelo this is a Test </p>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='checkout' element={<CheckoutPage />}></Route>
+        <Route path='orders' element={<OrdersPage />}></Route>
+        <Route path='tracking' element={<TrackingPage />}></Route>
+      </Routes>      
+    </>
+  );
+}
+// symbol / means empty path else name of the page
+// elem tell which component to display 
+
+export default App
