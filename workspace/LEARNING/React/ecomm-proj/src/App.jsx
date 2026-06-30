89181a68c2a05f542248e5b9cb0d9fcef6514c3d
itsvroy007 <itsvroy007@duck.com>
2026-06-30 23:25:00 +0530

    Generated Html for orders and deliveryoptions

--- workspace/LEARNING/React/ecomm-proj/src/App.jsx
+++ workspace/LEARNING/React/ecomm-proj/src/App.jsx
@@ -1,20 +1,20 @@
 import axios from 'axios'
 import {useEffect , useState } from 'react'
 import { Routes,Route } from 'react-router'
-import { HomePage } from './Pages/HomePage'
-import { CheckoutPage } from './Pages/CheckoutPage'
-import { OrdersPage } from './Pages/OrdersPage'
+import { HomePage } from './Pages/home/HomePage'
+import { CheckoutPage } from './Pages/checkout/CheckoutPage'
+import { OrdersPage } from './Pages/orders/OrdersPage'
 import { TrackingPage } from './Pages/TrackingPage' 
 import './App.css'
 
 function App() {
     const [ cart , setCart] = useState([]);
     useEffect(() => {
-      axios.get('http://localhost:3000/api/cart-items?expand=product') // ?expand=product give prod details 
-        .then((response) => {
-          setCart(response.data);
-          
-        });
+      const fetchAppData = async () => {
+        const response = await axios.get('http://localhost:3000/api/cart-items?expand=product') // ?expand=product give prod details 
+        setCart(response.data);
+      };
+      fetchAppData();
     },[]);
 
   return (
@@ -22,10 +22,9 @@ function App() {
       <Routes>
           <Route path='/' element={<HomePage cart={cart}/>}></Route>
           <Route path='checkout' element={<CheckoutPage cart={cart} />}></Route>
-          <Route path='orders' element={<OrdersPage />}></Route>
+          <Route path='orders' element={<OrdersPage cart={cart} />}></Route>
           <Route path='tracking' element={<TrackingPage />}></Route>
       </Routes>
-      
     </>
   )
 }
--- workspace/LEARNING/React/ecomm-proj/vite.config.js
+++ workspace/LEARNING/React/ecomm-proj/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   server:{
     proxy:{
       '/api':{
@@ -13,7 +12,6 @@ export default defineConfig({
       '/images' :{
         target:'http://localhost:3000'
       }
-
     }
   }
 })
